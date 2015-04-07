ea0cf86c6aa3d955eeafb4c8a26266f039ded388
Sabya <sabyasachi.exe@gmail.com>
2015-04-06 21:49:57 -0400

    Adding open in CRX DE Button. And made some budfixes .

--- popup.js
+++ popup.js
@@ -1,22 +1,24 @@
 var currentUrl;
 var activatedNodes = new Array();
+var contentPageURL;
+var relativePagePath;
 
-chrome.tabs.getSelected(null, function(tab) {
+chrome.tabs.getSelected(null, function (tab) {
 
     // Global Variables . will be set when the extension loads.
-    // Will be used accross functionalities.
+    // Will be used across functionality
 
-    var host = "" ;
-    var port = "" ;
-    var scheme = "" ;
-    var actualAEMPageURL = "" ;
-    var actualPageURLWithoutQueryParam = "" ;
-    var firstPart = "" ;
+    var host = "";
+    var port = "";
+    var scheme = "";
+    var actualAEMPageURL = "";
+    var actualPageURLWithoutQueryParam = "";
+    var firstPart = "";
 
     var imagePath = chrome.extension.getURL('icon.png');
     console.log('image path is :' + imagePath);
 
-    $('#fadfishImage').attr('src',imagePath);
+    $('#fadfishImage').attr('src', imagePath);
 
     console.log('Extension clicked');
 
@@ -27,12 +29,19 @@ chrome.tabs.getSelected(null, function(tab) {
 
 
     // Handle quick link clicks.
-    $('.quick_button').click(function(){
-       url = $(this).data('url');
-       console.log('quick links clicked .. ' + url);
+    $('.quick_button').click(function () {
+        url = $(this).data('url');
+        console.log('quick links clicked .. ' + url);
 
-       window.open(url, '_blank');
+        window.open(url, '_blank');
     });
+
+    $('#open-in-crxde').click(function () {
+        url = $(this).data('url');
+        console.log('quick links clicked .. ' + url);
+
+        window.open(url, '_blank');
+    })
 
     // Handle WCM Mode Switcher.
 
@@ -40,21 +49,24 @@ chrome.tabs.getSelected(null, function(tab) {
         // actualPageURLWithoutQueryParam will not have any query string present.
         // Relax and append wcmmode to it.
 
-        currentPageURL = actualPageURLWithoutQueryParam + '.html' ;
+        currentPageURL = actualPageURLWithoutQueryParam + '.html';
         console.log('current page url is : ' + currentPageURL);
-
-        if((desiredMode === 'edit' || desiredMode === 'design') &&  currentPageURL.indexOf('/cf#') === -1){
-            currentPageURL = currentPageURL.replace(firstPart,firstPart + '/cf#');
-        }
         var desiredMode = $(this).attr('id');
         console.log('desired mode is ' + desiredMode);
+        if ((desiredMode === 'edit' || desiredMode === 'design') && currentPageURL.indexOf('/cf#') === -1) {
+            currentPageURL = currentPageURL.replace(firstPart, firstPart + '/cf#');
+        } else if (desiredMode === 'disabled') {
+            currentPageURL = currentPageURL.replace('/cf#', "");
+        }
 
-        currentPageURL = currentPageURL.concat('?wcmmode='+desiredMode);
+        currentPageURL = currentPageURL.concat('?wcmmode=' + desiredMode);
 
         console.log('calculated current page url is : ' + currentPageURL);
 
-        window.open(currentPageURL); // http://localhost:4502/content/leggmason/en_us/insights/market-outlook/chinese-growth?wcmmode=design
-        //window.open(currentPageURL, '_top');
+        chrome.tabs.getSelected(null, function (tab) {
+            chrome.tabs.update(tab.id, {url: currentPageURL});
+        });
+
     });
 
 
@@ -64,7 +76,7 @@ chrome.tabs.getSelected(null, function(tab) {
     console.log('url value is : ' + webgrp);
     // url value is : http://localhost:4502/content/fadfish/home.html,http://localhost:4502/content/fadfish/home,html
 
-    actualAEMPageURL = webgrp ; // Setting value of global var. Will be used by other functionalities.
+    actualAEMPageURL = webgrp; // Setting value of global var. Will be used by other functionalities.
 
     if (webgrp.length > 0) {
         //var pageURL = webgrp[1] + '.infinity.json'; // Get the inifinity json of that page if its exposed .
@@ -72,62 +84,60 @@ chrome.tabs.getSelected(null, function(tab) {
 
         console.log('page url : ' + pageURL);
 
-        actualPageURLWithoutQueryParam = webgrp[1] ;
+        actualPageURLWithoutQueryParam = webgrp[1];
         // Extract Host and Port .
 
 
-        var tempURL = pageURL ;
+        var tempURL = pageURL;
         var disectedURL = tempURL.split('/');
 
         scheme = disectedURL[0];
-        host  = disectedURL[2];
+        host = disectedURL[2];
 
-        //var relativePagePath = disectedURL[3] ;
-        //console.log('relative page path is : ' + relativePagePath);
-
-        //console.log('protocol ' + protocol.join('/'));
-
-
-
-        firstPart = scheme + "//" + host ;
+        firstPart = scheme + "//" + host;
 
         console.log('first part of the url is : ' + firstPart);
 
-        if(pageURL.indexOf('cf#') > 0){
+        if (pageURL.indexOf('cf#') > 0) {
             console.log('User has opened the page with content finder');
-            pageURL = pageURL.replace("/cf#","");
+            pageURL = pageURL.replace("/cf#", "");
             console.log('page url after chopping off the cf# from url : ' + pageURL);
-        }else{
+        } else if (pageURL.indexOf('/editor.html') > 0) {
+            pageURL = pageURL.replace("/editor.html", "");
+        } else {
             console.log('User has opened the page in publish mode');
         }
+        relativePagePath = pageURL;
+        relativePagePath = relativePagePath.replace(firstPart, "");
+        relativePagePath = relativePagePath.substring(0, relativePagePath.indexOf("."));
+        console.log('relative page path :: ' + relativePagePath);
 
 
         // pageURL == http://localhost:4502/content/fadfish/home.tidy.-1.json
 
         $.ajax({
-            url : pageURL,
-            async : true,
-            type : 'GET',
-            dataType : "json",
-            timeout : 5500,
+            url: pageURL,
+            async: true,
+            type: 'GET',
+            dataType: "json",
+            timeout: 5500,
             statusCode: {
-                200: function(data, status, request) {
+                200: function (data, status, request) {
 
                     console.log('status is : ' + status);
                     console.log('data from AEM is : ' + data);
 
-
-                    createQuickLinks(tab,firstPart);
+                    createQuickLinks(tab, firstPart, relativePagePath);
                     getPageInformation(tab, data);
                     getSkimmedPageInformation(tab, data);
                 }
             },
-            success : function(data, status, request) {
-                console.log('Inside success !!') ;
+            success: function (data, status, request) {
+                console.log('Inside success !!');
             },
-            error : function (xhr, ajaxOptions, thrownError) {
-                console.log('Its screwed ! Something wrong happened!!') ;
-                $('#error_fadfish').text('Sorry ! No Data is available for this page');
+            error: function (xhr, ajaxOptions, thrownError) {
+                console.log('Its screwed ! Something wrong happened!!');
+                $('.error_fadfish').text('Sorry ! No Data is available for this page');
             }
         });
     }
@@ -135,10 +145,9 @@ chrome.tabs.getSelected(null, function(tab) {
 });
 
 
-
 <!-- Create Quick Links starts here -->
 
-function createQuickLinks(tab,dynamicPart){
+function createQuickLinks(tab, dynamicPart, relativePagePath) {
 
     console.log('creating quick links for AEM Viewer');
     var welcomePageURL = dynamicPart + '/welcome';
@@ -152,18 +161,21 @@ function createQuickLinks(tab,dynamicPart){
     var packagemanager = dynamicPart + '/crx/packmgr';
     var workflowconsole = dynamicPart + '/libs/cq/workflow/content/console.html';
     var slinglogs = dynamicPart + '/system/console/status-slinglogs';
+    var openInCRXDEURL = dynamicPart + '/crx/de/index.jsp#' + relativePagePath;
+    console.log('Open In CRX De URL :: ' + openInCRXDEURL);
 
-    $('#aemwelcome').attr('data-url',welcomePageURL) ;
-    $('#siteAdmin').attr('data-url',siteAdmin) ;
-    $('#crxde').attr('data-url',crxde) ;
-    $('#sysconsole').attr('data-url',sysconsole) ;
-    $('#crxexplorer').attr('data-url',crxexplorer) ;
-    $('#configMgr').attr('data-url',configMgr) ;
-    $('#resourceResolverConfig').attr('data-url',resourceResolverConfig) ;
-    $('#resourceResolverTest').attr('data-url',resourceResolverTest) ;
-    $('#packagemanager').attr('data-url',packagemanager) ;
-    $('#workflowconsole').attr('data-url',workflowconsole) ;
-    $('#slinglogs').attr('data-url',slinglogs) ;
+    $('#aemwelcome').attr('data-url', welcomePageURL);
+    $('#siteAdmin').attr('data-url', siteAdmin);
+    $('#crxde').attr('data-url', crxde);
+    $('#sysconsole').attr('data-url', sysconsole);
+    $('#crxexplorer').attr('data-url', crxexplorer);
+    $('#configMgr').attr('data-url', configMgr);
+    $('#resourceResolverConfig').attr('data-url', resourceResolverConfig);
+    $('#resourceResolverTest').attr('data-url', resourceResolverTest);
+    $('#packagemanager').attr('data-url', packagemanager);
+    $('#workflowconsole').attr('data-url', workflowconsole);
+    $('#slinglogs').attr('data-url', slinglogs);
+    $('#open-in-crxde').attr('data-url', openInCRXDEURL);
 
 }
 
@@ -175,11 +187,11 @@ function createQuickLinks(tab,dynamicPart){
 
 <!-- Create the RAW Data here starts here -->
 
-var getPageInformation = function(tab, data) {
-    $(function() {
+var getPageInformation = function (tab, data) {
+    $(function () {
         $("#tree").dynatree(
             {
-                onDblClick : function(node, event) {
+                onDblClick: function (node, event) {
                     if (isProp(node)) {
                         var cmpUrl = currentUrl
                             + createURL(node.getParent()
@@ -188,13 +200,13 @@ var getPageInformation = function(tab, data) {
                     }
                 },
 
-                onClick : function(node, event) {
+                onClick: function (node, event) {
                     if (event.altKey && isComp(node)) {
                         var cmpUrl = currentUrl
                             + createURL(node, '') + '.html';
                         chrome.tabs.create({
-                            'url' : cmpUrl,
-                            'selected' : true
+                            'url': cmpUrl,
+                            'selected': true
                         });
                     }
                 }
@@ -206,7 +218,7 @@ var getPageInformation = function(tab, data) {
     createDynatree(obj, data);
     $("#tree").dynatree("getRoot").addChild(obj);
 
-    $("#tree").dynatree("getRoot").visit(function(node) {
+    $("#tree").dynatree("getRoot").visit(function (node) {
 
         if (isActivatedNode(node)) {
             node.expand(true);
@@ -220,11 +232,11 @@ var getPageInformation = function(tab, data) {
 
 <!-- Create the Skimmed Data starts here -->
 
-var getSkimmedPageInformation = function(tab, data) {
-    $(function() {
+var getSkimmedPageInformation = function (tab, data) {
+    $(function () {
         $("#treeskimmed").dynatree(
             {
-                onDblClick : function(node, event) {
+                onDblClick: function (node, event) {
                     if (isProp(node)) {
                         var cmpUrl = currentUrl
                             + createURL(node.getParent()
@@ -233,13 +245,13 @@ var getSkimmedPageInformation = function(tab, data) {
                     }
                 },
 
-                onClick : function(node, event) {
+                onClick: function (node, event) {
                     if (event.altKey && isComp(node)) {
                         var cmpUrl = currentUrl
                             + createURL(node, '') + '.html';
                         chrome.tabs.create({
-                            'url' : cmpUrl,
-                            'selected' : true
+                            'url': cmpUrl,
+                            'selected': true
                         });
                     }
                 }
@@ -251,7 +263,7 @@ var getSkimmedPageInformation = function(tab, data) {
     createSkimmedDynatree(obj, data);
     $("#treeskimmed").dynatree("getRoot").addChild(obj);
 
-    $("#treeskimmed").dynatree("getRoot").visit(function(node) {
+    $("#treeskimmed").dynatree("getRoot").visit(function (node) {
 
         if (isActivatedNode(node)) {
             node.expand(true);
@@ -263,7 +275,7 @@ var getSkimmedPageInformation = function(tab, data) {
 
 <!-- Full Dyna tree -->
 
-var createURL = function(node, s) {
+var createURL = function (node, s) {
 
     if (node.data.title != null)
         s = '/' + node.data.title + s;
@@ -276,30 +288,28 @@ var createURL = function(node, s) {
 };
 
 
-
-
-var createDynatree = function(object, allData) {
+var createDynatree = function (object, allData) {
 
     var counter = 0;
-    for ( var k in allData) {
+    for (var k in allData) {
         var title = k;
         title = '<div>' + title + '</div>';
 
         if (typeof (allData[k]) != 'object') {
             object[counter] = {
-                title : title,
-                isFolder : true,
+                title: title,
+                isFolder: true,
                 classNames: { }
             };
             object[counter].children = [];
             object[counter].children[0] = {
-                title : allData[k],
-                isFolder : false
+                title: allData[k],
+                isFolder: false
             };
         } else {
             object[counter] = {
-                title : title,
-                isFolder : true,
+                title: title,
+                isFolder: true
             };
             object[counter].children = [];
             createDynatree(object[counter].children, allData[k]);
@@ -309,93 +319,91 @@ var createDynatree = function(object, allData) {
 };
 
 
-
-
-var createSkimmedDynatree = function(object, allData) {
+var createSkimmedDynatree = function (object, allData) {
 
     var counter = 0;
-    for ( var k in allData) {
+    for (var k in allData) {
         var title = k;
 
-        if(title === 'jcr:content'){
-            title = "Properties" ;
-        }else if(title === 'jcr:mixinTypes'){
-            continue ;
-        }else if(title === 'jcr:createdBy'){
-            continue ;
-        }else if(title === 'jcr:created'){
-            continue ;
-        }else if(title === 'jcr:primaryType'){
-            continue ;
-        }else if(title === 'jcr:description'){
-            title = 'Meta Description' ;
-        }else if(title === 'sling:resourceType'){
-            title = 'Resource Type' ;
-        }else if(title === 'pageTitle'){
-            title = 'Browser Title' ;
-        }else if (title === 'metakeywords'){
+        if (title === 'jcr:content') {
+            title = "Properties";
+        } else if (title === 'jcr:mixinTypes') {
+            continue;
+        } else if (title === 'jcr:createdBy') {
+            continue;
+        } else if (title === 'jcr:created') {
+            continue;
+        } else if (title === 'jcr:primaryType') {
+            continue;
+        } else if (title === 'jcr:description') {
+            title = 'Meta Description';
+        } else if (title === 'sling:resourceType') {
+            title = 'Resource Type';
+        } else if (title === 'pageTitle') {
+            title = 'Browser Title';
+        } else if (title === 'metakeywords') {
             title = 'Meta Keywords';
-        }else if(title === 'cq:template'){
-            title = 'Template' ;
-        }else if(title === 'cq:lastReplicatedBy'){
-            title = 'Last Activator' ;
-        }else if(title === 'jcr:mixinTypes'){
-            continue ;
-        }else if(title === 'cq:lastReplicationAction'){
-            continue ;
-        }else if(title === 'cq:lastModifiedBy'){
-            title = 'Last Modifier' ;
-        }else if(title === 'cq:lastModified'){
-            title = 'Last Modified Date' ;
-        }else if(title === 'cq:lastReplicated'){
-            title = 'Last Activated Date' ;
-        }else if(title === 'jcr:created'){
+        } else if (title === 'cq:template') {
+            title = 'Template';
+        } else if (title === 'cq:lastReplicatedBy') {
+            title = 'Last Activator';
+        } else if (title === 'jcr:mixinTypes') {
             continue;
-        }else if(title === 'jcr:uuid'){
+        } else if (title === 'cq:lastReplicationAction') {
             continue;
-        }else if(title === 'jcr:baseVersion'){
+        } else if (title === 'cq:lastModifiedBy') {
+            title = 'Last Modifier';
+        } else if (title === 'cq:lastModified') {
+            title = 'Last Modified Date';
+        } else if (title === 'cq:lastReplicated') {
+            title = 'Last Activated Date';
+        } else if (title === 'jcr:created') {
             continue;
-        }else if(title === 'jcr:primaryType'){
+        } else if (title === 'jcr:uuid') {
             continue;
-        }else if(title === 'jcr:isCheckedOut'){
+        } else if (title === 'jcr:baseVersion') {
             continue;
-        }else if(title === 'jcr:predecessors'){
+        } else if (title === 'jcr:primaryType') {
             continue;
-        }else if(title === 'jcr:versionHistory'){
+        } else if (title === 'jcr:isCheckedOut') {
             continue;
-        }else if(title === 'jcr:title'){
-            title = 'Title in Siteadmin' ;
-        }else if(title == 'brand'){
+        } else if (title === 'jcr:predecessors') {
+            continue;
+        } else if (title === 'jcr:versionHistory') {
+            continue;
+        } else if (title === 'jcr:title') {
+            title = 'Title in Siteadmin';
+        } else if (title == 'brand') {
             title = 'Brand';
-        }else if(title == 'region'){
+        } else if (title == 'region') {
             title = 'Region';
-        }else if(title == 'role'){
-            title = 'Role' ;
-        }else if(title == 'status'){
-            title = 'Status' ;
+        } else if (title == 'role') {
+            title = 'Role';
+        } else if (title == 'status') {
+            title = 'Status';
         }
-        else{
+        else {
             console.log('inside else block');
-            continue ;
+            continue;
         }
 
         title = '<div>' + title + '</div>';
 
         if (typeof (allData[k]) != 'object') {
             object[counter] = {
-                title : title,
-                isFolder : true,
+                title: title,
+                isFolder: true,
                 classNames: { }
             };
             object[counter].children = [];
             object[counter].children[0] = {
-                title : allData[k],
-                isFolder : false
+                title: allData[k],
+                isFolder: false
             };
         } else {
             object[counter] = {
-                title : title,
-                isFolder : true
+                title: title,
+                isFolder: true
             };
             object[counter].children = [];
             createSkimmedDynatree(object[counter].children, allData[k]);
@@ -406,9 +414,7 @@ var createSkimmedDynatree = function(object, allData) {
 };
 
 
-
-
-var isActivatedNode = function(node) {
+var isActivatedNode = function (node) {
     return activatedNodes.indexOf(getPath(node)) > -1;
 };
 
@@ -423,8 +429,7 @@ function isComp(node) {
 };
 
 
-
-var getPath = function(node) {
+var getPath = function (node) {
     return createURL(node, '');
 };
 
